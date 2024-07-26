85eadb90a5e0c908dd8e3fa13c91798604860fdb
PhamHuy <163984306+PhamHuy092004@users.noreply.github.com>
2024-07-26 21:39:43 +0700

    Add project files.

---
+++ PhamQuocHuy_BE_ValidateObjectRequest/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhamQuocHuy_BE_ValidateObjectRequest.Model;
+using System.Collections.Generic;
+
+namespace PhamQuocHuy_BE_ValidateObjectRequest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private static List<Student> students = new List<Student>();
+
+        [HttpPost]
+        public IActionResult CreateStudent([FromBody] Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            students.Add(student);
+
+            return Ok($"Đã thêm {student.FullName} vào danh sách");
+        }
+
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            return Ok(students);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetStudent(string name)
+        {
+            var student = students.FirstOrDefault(s => s.FullName == name);
+
+            if (student == null)
+            {
+                return NotFound("Student not found.");
+            }
+
+            return Ok(student);
+        }
+    }
+}
---
+++ PhamQuocHuy_BE_ValidateObjectRequest/Model/Student.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhamQuocHuy_BE_ValidateObjectRequest.Model
+{
+    public class Student
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int StudentID { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập đầy đủ họ tên")]
+        public string FullName { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập giới tính")]
+        [EnumDataType(typeof(Gendertype), ErrorMessage = "Vui lòng nhập đúng")]
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập điểm")]
+        [Range(0,10,ErrorMessage = "Điểm chỉ từ 0 đến 10")]
+        public decimal Point { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập địa chỉ")]
+        [EnumDataType(typeof(Province), ErrorMessage = "Tỉnh thành không hợp lệ")]
+        public string Province { get; set; }
+
+    }
+
+    public enum Gendertype
+    {
+        Nam, Nữ, Khác
+    }
+    public enum Province
+    {
+        AnGiang,
+        BaRiaVungTau,
+        BacGiang,
+        BacKan,
+        BacLieu,
+        BacNinh,
+        BenTre,
+        BinhDinh,
+        BinhDuong,
+        BinhPhuoc,
+        BinhThuan,
+        CaMau,
+        CanTho
+    }
+
+}
